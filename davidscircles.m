3fcea727615c0aab38fbb7151c1b10e33b26325b
David :) <davskim@umich.edu>
2022-02-01 09:25:18 -0500

    David's Circles
    Added another actigraphy-related feature that plots circular statistics. File is called davidscircles.

---
+++ davidscircles.m
@@ -0,0 +1,29 @@
+%David's Circles :DDD
+% Let's go
+
+shortname = curr_activity_timetable_hourly_binned_mean;
+clockdat = zeros(1440,1);
+
+for j = 0:1439
+    for i = find((shortname.HrOfDay*60) + shortname.Time.Minute == j)
+      clockdat(j+1)= sum(shortname.NumChangedPixels(i));
+    end
+end
+figure;
+subplot(1,2,1);
+bar(clockdat');
+
+histcounts = [];
+for i = 1:length(normclockdat)
+    histcounts = [histcounts; i*ones(round(clockdat(i)),1)];
+end
+subplot(1,2,2);
+histogram(histcounts);
+
+figure;
+subplot(1,2,1);
+%PvalueOutput = circ_rtest(histcounts);
+polarhistogram(2*pi*(histcounts/1440) - (pi/1440),24);
+%title(['P = ' num2str(round(PvalueOutput,3))]);
+subplot(1,2,2);
+polarhistogram((2*pi*(histcounts/1440)) - (pi/1440),1440);
