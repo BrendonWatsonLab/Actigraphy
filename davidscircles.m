c1d287ab2753c3e94f9b501cf14a381fde6f3ae8
David Kim <davskim@umich.edu>
2022-02-16 08:09:45 -0500

    Overlaying Arrow
    This is the functionality that adds in an overlayed arrow into the polar plots. It points it in the direction of the mean vector.

--- davidscircles.m
+++ davidscircles.m
@@ -14,24 +14,45 @@ clockdat = zeros(1440,1);
 
 for j = 0:1439
     for i = find((shortname.HrOfDay*60) + shortname.Time.Minute == j)
-      clockdat(j+1)= sum(shortname.NumChangedPixels(i));
+       temp = shortname.NumChangedPixels(i);
+      clockdat(j+1)= sum(temp(~isnan(shortname.NumChangedPixels(i))));
     end
 end
 figure;
 subplot(1,2,1);
 bar(clockdat');
+title('Minutely Binned');
+
+%normalizing (optional);
+clockdat = round((clockdat / min(clockdat)) * 10);
 
 histcounts = [];
 for i = 1:length(clockdat)
     histcounts = [histcounts; i*ones(round(clockdat(i)),1)];
 end
 subplot(1,2,2);
-histogram(histcounts);
+histogram(histcounts,24);
+title('Hourly Binned');
+
+RayleighPval = circ_rtest(2*pi*(histcounts/1440) - (pi/1440));
+VecLength = circ_r(2*pi*(histcounts/1440) - (pi/1440));
+VecDir = circ_mean(2*pi*(histcounts/1440) - (pi/1440));
 
 figure;
 subplot(1,2,1);
-%PvalueOutput = circ_rtest(histcounts);
 polarhistogram(2*pi*(histcounts/1440) - (pi/1440),24);
-%title(['P = ' num2str(round(PvalueOutput,3))]);
+hold on;
+overlayingarrow;
+hold off;
+title('Hourly Binned')
+thetaticks(0:15:345)
+thetaticklabels({'00:00','01:00','02:00','03:00','04:00','05:00','06:00','07:00','08:00','09:00','10:00','11:00','12:00','13:00','14:00','15:00','16:00','17:00','18:00','19:00','20:00','21:00','22:00','23:00'})
+
 subplot(1,2,2);
 polarhistogram((2*pi*(histcounts/1440)) - (pi/1440),1440);
+hold on;
+overlayingarrow;
+hold off;
+title('Minutely Binned');
+thetaticks(0:15:345)
+sgtitle('24hour Plotted NumChangedPixels')
